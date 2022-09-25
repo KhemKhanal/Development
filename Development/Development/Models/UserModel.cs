d87afcc1c7dd9856e23a4522299470f508288e4d
khemk <khemk@LENOVOBOOK>
2022-09-25 16:13:47 +0700

    Add project files.

---
+++ Development/Development/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Development.Models
+{
+    public class UserModel
+    {
+    }
+}
---
+++ Development/Development/ViewModels/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Development.ViewModels
+{
+    public class UserViewModel
+    {
+    }
+}
