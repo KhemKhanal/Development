45707387f5ba540c1f6e2afa656acc3b8a11aefd
Nawarat Chatree <112376464+NawaratKhem@users.noreply.github.com>
2024-02-26 16:43:18 +0700

    Model for saving data

---
+++ Development/Development/Models/Calculations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Development.Models
+{
+    public class Calculations
+    {
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Showw { get; set; }
+        public string Answerr { get; set; }
+        
+
+        public Calculations(string date, string time, string show, string answer) 
+        {
+            Date = date;
+            Time = time;
+            Showw = show; 
+            Answerr = answer;
+        }
+
+        public Calculations() { }
+
+
+    }
+}
